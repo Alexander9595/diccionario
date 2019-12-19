21f24b97afb7d7d26ae79c1a3214f10947e7c43d
Alexander9595 <gangalecarlos14@gmail.com>
2019-12-19 08:38:04 -0500

    subir archivo

---
+++ CRUD/Form1.Designer.vb
@@ -0,0 +1,142 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnagre = New System.Windows.Forms.Button()
+        Me.btnactu = New System.Windows.Forms.Button()
+        Me.btnborra = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'btnagre
+        '
+        Me.btnagre.Location = New System.Drawing.Point(390, 76)
+        Me.btnagre.Name = "btnagre"
+        Me.btnagre.Size = New System.Drawing.Size(75, 23)
+        Me.btnagre.TabIndex = 0
+        Me.btnagre.Text = "AGREAGAR"
+        Me.btnagre.UseVisualStyleBackColor = True
+        '
+        'btnactu
+        '
+        Me.btnactu.Location = New System.Drawing.Point(609, 76)
+        Me.btnactu.Name = "btnactu"
+        Me.btnactu.Size = New System.Drawing.Size(88, 23)
+        Me.btnactu.TabIndex = 1
+        Me.btnactu.Text = "ACTUALIZAR"
+        Me.btnactu.UseVisualStyleBackColor = True
+        '
+        'btnborra
+        '
+        Me.btnborra.Location = New System.Drawing.Point(507, 76)
+        Me.btnborra.Name = "btnborra"
+        Me.btnborra.Size = New System.Drawing.Size(75, 23)
+        Me.btnborra.TabIndex = 2
+        Me.btnborra.Text = "ELIMINAR"
+        Me.btnborra.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(326, 24)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(77, 13)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "DICCIONARIO"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(137, 78)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(188, 20)
+        Me.TextBox1.TabIndex = 4
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.Location = New System.Drawing.Point(137, 172)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(457, 134)
+        Me.ListBox1.TabIndex = 5
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(137, 125)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(188, 20)
+        Me.TextBox2.TabIndex = 6
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(43, 78)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(56, 13)
+        Me.Label2.TabIndex = 7
+        Me.Label2.Text = "PALABRA"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(43, 128)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(75, 13)
+        Me.Label3.TabIndex = 8
+        Me.Label3.Text = "SIGNIFICADO"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(709, 450)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.btnborra)
+        Me.Controls.Add(Me.btnactu)
+        Me.Controls.Add(Me.btnagre)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnagre As Button
+    Friend WithEvents btnactu As Button
+    Friend WithEvents btnborra As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+End Class
---
+++ CRUD/Form1.vb
@@ -0,0 +1,28 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles btnagre.Click
+        ListBox1.Items.Add(TextBox1.Text & "   " & TextBox2.Text)
+        TextBox2.Text = ""
+        TextBox1.Text = ""
+
+
+
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles btnborra.Click
+        ListBox1.Items.RemoveAt(ListBox1.SelectedIndex())
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles btnactu.Click
+        Dim dato As String = ListBox1.SelectedIndex
+        Dim dato2 As String = ListBox1.SelectedIndex
+        ListBox1.Items.RemoveAt(dato & dato2)
+        ListBox1.Items.Insert(dato, TextBox1.Text)
+        ListBox1.Items.Insert(dato2, TextBox2.Text)
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
